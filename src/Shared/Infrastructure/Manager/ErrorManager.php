87e7afc46e16ffd0001bcc105fb6fa619892918a
joep <joepdooper@hotmail.com>
2026-09-08 22:15:35 +0200

    Update

--- src/Shared/Infrastructure/Manager/ErrorManager.php
+++ src/Shared/Infrastructure/Manager/ErrorManager.php
@@ -12,9 +12,20 @@ class ErrorManager
         $reporting = defined($reporting) ? constant($reporting) : E_ALL;
         error_reporting($reporting);
 
-        ini_set('ignore_repeated_errors', $_ENV['IGNORE_REPEATED_ERRORS'] ?: 'true');
-        ini_set('display_errors', $_ENV['DISPLAY_ERRORS'] ?: 'false');
-        ini_set('log_errors', $_ENV['LOG_ERRORS'] ?: 'true');
+        ini_set(
+            'ignore_repeated_errors',
+            filter_var($_ENV['IGNORE_REPEATED_ERRORS'] ?? true, FILTER_VALIDATE_BOOL)
+        );
+
+        ini_set(
+            'display_errors',
+            filter_var($_ENV['DISPLAY_ERRORS'] ?? false, FILTER_VALIDATE_BOOL)
+        );
+
+        ini_set(
+            'log_errors',
+            filter_var($_ENV['LOG_ERRORS'] ?? true, FILTER_VALIDATE_BOOL)
+        );
 
         $logFile = $_ENV['ERROR_LOG_FILE'];
         if ($logFile) {
